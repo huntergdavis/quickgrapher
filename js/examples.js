f5526b0c64d6c2945b8229e4383cdbf0fc08ac68
root <root@puppypc.(none)>
2010-11-08 01:59:34 +0800

    added the rest of the examples

--- js/examples.js
+++ js/examples.js
@@ -67,137 +67,96 @@ Examples.push({
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Population Growth",
+    fxn : "LastYearPopulation + LastYearPopulation*(ReprodiveRate - Attrition)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Fiber Optic Response Time",
+    fxn : "ProcessingTime + (2*Distance)/(200*(1000^2))"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Watts",
+    fxn : "Amps * Volts"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Velocity",
+    fxn : "InitialVelocity + Acceleration*Time"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Position of a Moving Object",
+    fxn : "InitialPosition + InitialVelocity*Time + 1/2 * acceleration * Time^2"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "torque",
+    fxn : "radius * force * sin(angle)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Surface area of cylinder",
+    fxn : "2*pi*radius*length + 2*pi*radius^2"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "First Order Derivitive of X^N",
+    fxn : "n*x^(n-1)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "First Order Integral of x^N dx (real)",
+    fxn : "1/(n+1) * x^(n+1)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Glomerular filtration rate (Creatinine Clearance CCr) (Kidney Filtration Rate)",
+    fxn : "(CreatineConcentration * FlowRate)/PlasmaConcentration"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "IPAT Environmental Impact Equation",
+    fxn : "Population * Affluence * Technology"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Vertical Change Delta for a Weighted String between 2 Points",
+    fxn : "sqrt(((StringLength + PointsDistance)/2)*((StringLength - PointsDistance)/2))"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Gaussian",
+    fxn : "a * e^(-1*((x-b)^2/2*c^2))"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Mechanical Work",
+    fxn : "1/2 * mass * (secondVelocity^2 - firstVelocity^2)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Procrastination Equation",
+    fxn : "ExpectationOfSuccess*ValueofCompletion / (ImmediacyOfTask * PersonalDelaySensitivity)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Pressure Loss with friction coefficient .019",
+    fxn : ".019 * (length / hydraulicDiameter) * (density * velocity^2 /2)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Time Dilation qith Regard to Speed of Observer",
+    fxn : "sqrt(1-(velocity^2/(300000)^2))"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Distance Between 2 Points in 2D space on Grid",
+    fxn : "sqrt((xTwo-xOne)^2 + (yTwo-yOne)^2)"
 });
 
 Examples.push({
-    name: "",
-    fxn : ""
+    name: "Quadratic Equation (First Solution)",
+    fxn : "(-x + sqrt(b^2 - 4*a*c))/2*a"
 });
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
-Examples.push({
-    name: "",
-    fxn : ""
-});
-
