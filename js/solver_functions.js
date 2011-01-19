36d1396e30b36343eebff98d79e774240c8662b9
Mark Christensen <mark@discursivelabs.com>
2011-01-19 00:19:01 -0800

    Moved function definitions to new file

---
+++ js/solver_functions.js
@@ -0,0 +1,32 @@
+/**
+ * Requires ComplexFunctions (in solver.js)
+ */
+
+/* Defined functions */
+// Arithmetic
+Functions["+"] = new ComplexFunction(0,false, 
+        function(a,b) {
+            return a + b;
+        });
+Functions["-"] = new ComplexFunction(0,false,
+        function(a,b) {
+            return a - b;
+        });
+Functions["*"] = new ComplexFunction(1,false,
+        function(a,b) {
+            return a * b;
+        });
+Functions["/"] = new ComplexFunction(1,false,
+        function(a,b) {
+            return a / b;
+        });
+Functions["^"] = new ComplexFunction(2,false,
+        function(a,b) {
+            return Math.pow(a,b);
+        });
+        
+// Sinusoidal
+Functions["sin"] = new ComplexFunction(1,true,
+        function(a) {
+            return Math.sin(a);
+        });
