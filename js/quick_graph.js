eb80c479c119f68ecc13d432ba902081ab091641
Mark Christensen <mark@discursivelabs.com>
2011-01-25 13:23:36 -0800

    DRY'd the example list item creation

--- js/quick_graph.js
+++ js/quick_graph.js
@@ -1060,9 +1060,7 @@ function nextExamples()
         for(var i = (curr_page * 5); i < exLen && i < (curr_page + 1)*5; i++)
         {
             ex = examples[i];
-            example = document.createElement("li");
-            example.innerHTML == ex.name + " - " + ex.fxn;
-            list.append(example);
+            createExampleLink(ex, list);
         }
             
         if( curr_page > 0 )
@@ -1091,9 +1089,7 @@ function prevExamples()
         for(var i = (curr_page * 5); i < exLen && i < (curr_page + 1)*5; i++)
         {
             ex = examples[i];
-            example = document.createElement("li");
-            example.innerHTML == ex.name + " - " + ex.fxn;
-            list.append(example);
+            createExampleLink(ex, list);
         }
         
         $("#nextExamples").show();
@@ -1131,6 +1127,20 @@ function compressName(name)
     return compressed;
 }
 
+function createExampleLink(example, parent)
+{
+    var ex = document.createElement("li"),
+        inner = "<a href='/?";
+    inner += compressName(example.fxn);
+    inner += "'>"
+    inner += example.name;
+    inner += "</a>";
+    ex.innerHTML = inner;
+    ex.setAttribute("id","example_" + compressName(example.name));
+    //example.setAttribute("onclick","loadExample(this.id)");
+    parent.append(example);
+}
+
 function resetExamples()
 {
     curr_page = 0;
@@ -1146,16 +1156,7 @@ function resetExamples()
     for(var i = (curr_page * 5); i < exLen && i < (curr_page + 1)*5; i++)
     {
         ex = examples[i];
-        example = document.createElement("li");
-        var inner = "<a href='/?";
-        inner += compressName(ex.fxn);
-        inner += "'>"
-        inner += ex.name;
-        inner += "</a>";
-        example.innerHTML = inner;
-        example.setAttribute("id","example_" + compressName(ex.name));
-        //example.setAttribute("onclick","loadExample(this.id)");
-        list.append(example);
+        createExampleLink(ex, list);
     }
         
     if( exLen > 5 )
