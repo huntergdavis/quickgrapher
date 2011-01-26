0377ec0ef8becb429efbb9ef8ad72159380bb766
hunter@discursivelabs.com <hunter@discursivelabs.com>
2011-01-26 14:32:15 -0800

    updating examples

--- js/examples.js
+++ js/examples.js
@@ -23,149 +23,178 @@ Examples.push({
 Examples.push({
     name: "Gravity",
     fxn : "GravConstant * massOne * massTwo / distance^2",
-    URL : ""
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?GravConstant*massOne*massTwo/distance^2=0:0:0:0:{20:100:100:100:}0.01:1:1:1:[8.73:29:19:3:;1:1:1:1:'Gravity]"
 });
 
 Examples.push({
     name: "Einstein E=mc2",
-    fxn : "mass*(300*1000*1000)^2"
+    fxn : "mass*(300*1000*1000)^2",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?mass*(300*1000*1000)^2=0:{100:}1:[20:;1:'Einstein]"
 });
 
 Examples.push({
     name: "area of circle",
-    fxn : "pi * radius^2"
+    fxn : "pi * radius^2",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?pi*radius^2=0:{100:}1:[13:;1:'Circle%20Area]"
 });
 
 Examples.push({
     name: "circumference of a circle",
-    fxn : "2*pi*radius"
+    fxn : "2*pi*radius",
+    URL : ""
 });
 
 Examples.push({
     name: "Flake Equation (XKCD)",
-    fxn : "7*1000^3 * (CrazyPeople + Misinterpret)/10000 * TellProbability/10 * NumberPeopleTold * FirstHandRetellings * IgnoredDataProbability/10 * WiderAudienceFraction/100"
+    fxn : "7*1000^3 * (CrazyPeople + Misinterpret)/10000 * TellProbability/10 * NumberPeopleTold * FirstHandRetellings * IgnoredDataProbability/10 * WiderAudienceFraction/100",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?7*1000^3*(CrazyPeople+Misinterpret)/10000*TellProbability/10*NumberPeopleTold*FirstHandRetellings*IgnoredDataProbability/10*WiderAudienceFraction/100=0:0:0:0:0:0:0:{10:10:10:100:100:10:100:}1:1:1:1:1:1:1:[1:1:1:10:10:9:1:;1:1:1:1:1:1:1:'Flake%20Equation%20(XKCD)]"
 });
 
 Examples.push({
     name: "Only Date Above (HIMYM/XKCD/OTHERS)",
-    fxn : "Age/2 + 7"
+    fxn : "Age/2 + 7",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?Age/2+7=18:{126:}1:[27:;1:'Who%20Can%20You%20Date]"
 });
 
 Examples.push({
     name: "TV Romantic Drama Equation (Gay) (XKCD)",
-    fxn : "AllCast*(AllCast - 1)/2 + SameSex*(SameSex - AllCast)"
+    fxn : "AllCast*(AllCast - 1)/2 + SameSex*(SameSex - AllCast)",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?AllCast*(AllCast-1)/2+SameSex*(SameSex-AllCast)=0:0:{100:100:}1:1:[45:25:;1:1:'XKCD-Same%20Sex%20Graph]"
 });
 
 Examples.push({
     name: "TV Romantic Drama Equation (Straight) (XKCD)",
-    fxn : "SameSex*(AllCast - SameSex)"
+    fxn : "SameSex*(AllCast - SameSex)",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?SameSex*(AllCast-SameSex)=0:0:{100:100:}1:1:[17:44:;1:1:'XKCD%20Opposite%20Sex%20Graph]"
 });
 
 Examples.push({
     name: "Post-Game Seasonal Batting Average",
-    fxn : "(SeasonBats * (SeasonalAverage/100) + Hits)/(SeasonBats + TimesAtBat)"
+    fxn : "(SeasonBats * (SeasonalAverage/100) + Hits)/(SeasonBats + TimesAtBat)",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?(SeasonBats*(SeasonalAverage/100)+Hits)/(SeasonBats+TimesAtBat)=0:0:0:0:{100:100:100:13:}1:1:1:1:[40:60:81:13:;1:1:1:1:'Post%20Game%20Batting%20Average]"
 });
 
 Examples.push({
     name: "Focal Length",
-    fxn : "(ObjectDistance * LensToImageDistance)/(ObjectDistance + LensToImageDistance)"
+    fxn : "(ObjectDistance * LensToImageDistance)/(ObjectDistance + LensToImageDistance)",
+    URL : "file:///home/programmer/Projects/sliders/quick-graph-less.html?(ObjectDistance*LensToImageDistance)/(ObjectDistance+LensToImageDistance)=0:0:{100:100:}1:1:[37:20:;1:1:'Focal%20Length]"
 });
 
 Examples.push({
     name: "Population Growth",
-    fxn : "LastYearPopulation + LastYearPopulation*(ReprodiveRate - Attrition)"
+    fxn : "LastYearPopulation + LastYearPopulation*(ReprodiveRate - Attrition)",
+    URL : ""
 });
 
 Examples.push({
     name: "Fiber Optic Response Time",
-    fxn : "ProcessingTime + (2*Distance)/(200*(1000^2))"
+    fxn : "ProcessingTime + (2*Distance)/(200*(1000^2))",
+    URL : ""
 });
 
 Examples.push({
     name: "Watts",
-    fxn : "Amps * Volts"
+    fxn : "Amps * Volts",
+    URL : ""
 });
 
 Examples.push({
     name: "Velocity",
-    fxn : "InitialVelocity + Acceleration*Time"
+    fxn : "InitialVelocity + Acceleration*Time",
+    URL : ""
 });
 
 Examples.push({
     name: "Position of a Moving Object",
-    fxn : "InitialPosition + InitialVelocity*Time + 1/2 * acceleration * Time^2"
+    fxn : "InitialPosition + InitialVelocity*Time + 1/2 * acceleration * Time^2",
+    URL : ""
 });
 
 Examples.push({
     name: "torque",
-    fxn : "radius * force * sin(angle)"
+    fxn : "radius * force * sin(angle)",
+    URL : ""
 });
 
 Examples.push({
     name: "Surface area of cylinder",
-    fxn : "2*pi*radius*length + 2*pi*radius^2"
+    fxn : "2*pi*radius*length + 2*pi*radius^2",
+    URL : ""
 });
 
 Examples.push({
     name: "First Order Derivitive of x^n",
-    fxn : "n*x^(n-1)"
+    fxn : "n*x^(n-1)",
+    URL : ""
 });
 
 Examples.push({
     name: "First Order Integral of x^n dx",
-    fxn : "1/(n+1) * x^(n+1) + C"
+    fxn : "1/(n+1) * x^(n+1) + C",
+    URL : ""
 });
 
 Examples.push({
     name: "Glomerular filtration rate (Creatinine Clearance CCr) (Kidney Filtration Rate)",
-    fxn : "(CreatineConcentration * FlowRate)/PlasmaConcentration"
+    fxn : "(CreatineConcentration * FlowRate)/PlasmaConcentration",
+    URL : ""
 });
 
 Examples.push({
     name: "IPAT Environmental Impact Equation",
-    fxn : "Population * Affluence * Technology"
+    fxn : "Population * Affluence * Technology",
+    URL : ""
 });
 
 Examples.push({
     name: "Vertical Change Delta for a Weighted String between 2 Points",
-    fxn : "sqrt(((StringLength + PointsDistance)/2)*((StringLength - PointsDistance)/2))"
+    fxn : "sqrt(((StringLength + PointsDistance)/2)*((StringLength - PointsDistance)/2))",
+    URL : ""
 });
 
 Examples.push({
     name: "Gaussian",
-    fxn : "a * e^(-1*((x-b)^2/2*c^2))"
+    fxn : "a * e^(-1*((x-b)^2/2*c^2))",
+    URL : ""
 });
 
 Examples.push({
     name: "Mechanical Work",
-    fxn : "1/2 * mass * (secondVelocity^2 - firstVelocity^2)"
+    fxn : "1/2 * mass * (secondVelocity^2 - firstVelocity^2)",
+    URL : ""
 });
 
 Examples.push({
     name: "Procrastination Equation",
-    fxn : "ExpectationOfSuccess*ValueofCompletion / (ImmediacyOfTask * PersonalDelaySensitivity)"
+    fxn : "ExpectationOfSuccess*ValueofCompletion / (ImmediacyOfTask * PersonalDelaySensitivity)",
+    URL : ""
 });
 
 Examples.push({
     name: "Pressure Loss with friction coefficient .019",
-    fxn : ".019 * (length / hydraulicDiameter) * (density * velocity^2 /2)"
+    fxn : ".019 * (length / hydraulicDiameter) * (density * velocity^2 /2)",
+    URL : ""
 });
 
 Examples.push({
     name: "Time Dilation qith Regard to Speed of Observer",
-    fxn : "sqrt(1-(velocity^2/(300000)^2))"
+    fxn : "sqrt(1-(velocity^2/(300000)^2))",
+    URL : ""
 });
 
 Examples.push({
     name: "Distance Between 2 Points in 2D space on Grid",
-    fxn : "sqrt((xTwo-xOne)^2 + (yTwo-yOne)^2)"
+    fxn : "sqrt((xTwo-xOne)^2 + (yTwo-yOne)^2)",
+    URL : ""
 });
 
 Examples.push({
     name: "Quadratic Equation (First Solution)",
-    fxn : "(-x + sqrt(b^2 - 4*a*c))/2*a"
+    fxn : "(-x + sqrt(b^2 - 4*a*c))/2*a",
+    URL : ""
 });
 Examples.push({
     name: "Parenthesis test",
-    fxn : "sum(b*sin((x^(n-1))*pi),n*sin((x^(n-1))*pi),x^(n-2),(7*x*n))=-180:0.5:0:{100:1.1:100:}0.5:0.001:0.01:[-111.5:1.069:45.660000000000004:"
+    fxn : "sum(b*sin((x^(n-1))*pi),n*sin((x^(n-1))*pi),x^(n-2),(7*x*n))=-180:0.5:0:{100:1.1:100:}0.5:0.001:0.01:[-111.5:1.069:45.660000000000004:",
+    URL : ""
 });
