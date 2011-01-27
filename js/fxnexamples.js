f3a370733fd08a18fc1c58e4cb5ef24d78085dfe
hunter@discursivelabs.com <hunter@discursivelabs.com>
2011-01-27 08:35:13 -0800

    added function examples list

--- js/fxnexamples.js
+++ js/fxnexamples.js
@@ -20,10 +20,167 @@ FxnExamples.push({
 });
 
 FxnExamples.push({
-    name: "Plus",
-    fxn : "x + y",
-    url : "x+y=0:0:{100:100:}1:1:[5:22:;1:1:=Plus%20Graph]"
+    name: "Division",
+    fxn : "x / y",
+    url : "x/y=1:1:{100:100:}1:1:[5:22:;1:1:=Divide%20Graph]"
 });
+
+FxnExamples.push({
+    name: "Power",
+    fxn : "x^y",
+    url : "x^y=0:0:{100:100:}1:1:[5:22:;1:1:=Power%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Square Root",
+    fxn : "sqrt(x)",
+    url : "sqrt(x)=0:{100:}1:[5:;1:=Sqrt%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Pi",
+    fxn : "x*pi",
+    url : "x*pi=0:{100:}1:[5:;1:=Pi%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Euler's Number e",
+    fxn : "x*e",
+    url : "x*e=0:{100:}1:[5:;1:=Euler%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Sin",
+    fxn : "sin(x)",
+    url : "sin(x)=0:{100:}1:[5:;1:=Sin%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Cos",
+    fxn : "cos(x)",
+    url : "cos(x)=0:{100:}1:[5:;1:=Cos%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Tan",
+    fxn : "tan(x)",
+    url : "tan(x)=0:{100:}1:[5:;1:=Tan%20Graph]"
+});
+
+FxnExamples.push({
+    name: "aSin",
+    fxn : "asin(x)",
+    url : "asin(x)=0:{100:}1:[5:;1:=aSin%20Graph]"
+});
+
+FxnExamples.push({
+    name: "aCos",
+    fxn : "acos(x)",
+    url : "acos(x)=0:{100:}1:[5:;1:=aCos%20Graph]"
+});
+
+FxnExamples.push({
+    name: "aTan",
+    fxn : "atan(x)",
+    url : "atan(x)=0:{100:}1:[5:;1:=aTan%20Graph]"
+});
+
+
+FxnExamples.push({
+    name: "Log",
+    fxn : "log(x)",
+    url : "log(x)=0:{100:}1:[5:;1:=Log%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Natural Log",
+    fxn : "ln(x)",
+    url : "ln(x)=0:{100:}1:[5:;1:=Natural%20Log%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Random",
+    fxn : "rand(x)",
+    url : "rand(x)=0:{100:}1:[5:;1:=Random%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Factorial",
+    fxn : "fact(x)",
+    url : "fact(x)=0:{100:}1:[5:;1:=Factorial%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Fibonacci",
+    fxn : "fib(x)",
+    url : "fib(x)=0:{100:}1:[5:;1:=Fibonacci%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Absolute Value",
+    fxn : "abs(x)",
+    url : "abs(x)=-100:{100:}1:[5:;1:=Absolute%20Value%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Ceiling",
+    fxn : "ceil(x + 0.4)",
+    url : "ceil(x + 0.4)=0:{100:}1:[5:;1:=Ceiling%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Floor",
+    fxn : "Floor(x + 0.6)",
+    url : "Floor(x + 0.6)=0:{100:}1:[5:;1:=Floor%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Less",
+    fxn : "less(x,60)",
+    url : "less(x,60)=0:{100:}1:[8:;1:=Less%20Graph]"
+});
+
+
+FxnExamples.push({
+    name: "Greater",
+    fxn : "greater(x,60)",
+    url : "greater(x,60)=0:{100:}1:[8:;1:=Greater%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Average",
+    fxn : "average(x,60,80,20)",
+    url : "average(x,60,80,20)=0:{100:}1:[8:;1:=Average%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Summation",
+    fxn : "sum(x,60,80,20,6,43,22)",
+    url : "sum(x,60,80,20,6,43,22)=0:{100:}1:[8:;1:=Summation%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Product",
+    fxn : "product(x,1,3,4)",
+    url : "product(x,1,3,4)=0:{100:}1:[8:;1:=Product%20Graph]"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
