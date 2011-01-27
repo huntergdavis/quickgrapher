2166cb8a12d68465a8f1ec3713ea1e43be71ee96
hunter@discursivelabs.com <hunter@discursivelabs.com>
2011-01-27 08:16:43 -0800

    added function examples list

---
+++ js/fxnexamples.js
@@ -0,0 +1,30 @@
+/* Function Examples
+ * */
+
+FxnExamples.push({
+    name: "Plus",
+    fxn : "x + y",
+    url : "x+y=0:0:{100:100:}1:1:[5:22:;1:1:=Plus%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Minus",
+    fxn : "x - y",
+    url : "x-y=0:0:{100:100:}1:1:[5:22:;1:1:=Minus%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Times",
+    fxn : "x * y",
+    url : "x*y=0:0:{100:100:}1:1:[5:22:;1:1:=Times%20Graph]"
+});
+
+FxnExamples.push({
+    name: "Plus",
+    fxn : "x + y",
+    url : "x+y=0:0:{100:100:}1:1:[5:22:;1:1:=Plus%20Graph]"
+});
+
+
+
+
