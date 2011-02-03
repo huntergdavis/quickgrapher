c609f31aa88938d978a809ce9affc1af195ff932
hunter@discursivelabs.com <hunter@discursivelabs.com>
2011-02-03 15:08:40 -0800

    added temp. conversion equation examples

--- js/plain/examples.js
+++ js/plain/examples.js
@@ -20,6 +20,7 @@ Examples.push({
     url : "principal*((rate/1200)*(1+rate/1200)^months)/((1+rate/1200)^months-1)=0:0:0:{200:100:100:}10:1:1:[110:9:23:;1:1:1:=Loan%20Calculator]"
 });
 
+
 Examples.push({
     name: "Force of Gravity",
     fxn : "GravConstant * massOne * massTwo / distance^2",
@@ -42,6 +43,18 @@ Examples.push({
     name: "Circumference of a Circle",
     fxn : "2*pi*radius",
     url : "2*pi*radius=0:{100:}1:[13:;1:=Circle%20Circumference]"
+});
+
+Examples.push({
+    name: "Celsius to Fahrenheit",
+    fxn : "(Fahrenheit - 32) * 5/9",
+    url : "(Fahrenheit-32)*5%2F9%3D0%3A%7B100%3A%7D1%3A%5B50%3A%3B1%3A%3DCelsius%5D"
+});
+
+Examples.push({
+    name: "Fahrenheit to Celsius",
+    fxn : "Celsius * 9/5 + 32",
+    url : "Celsius*9%2F5%E2%80%A032%3D0%3A%7B100%3A%7D1%3A%5B0%3A%3B1%3A%3DFahrenheit%5D"
 });
 
 Examples.push({
