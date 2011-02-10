925b9404880f5709583902a68a28092ebf3b55a1
hunter@discursivelabs.com <hunter@discursivelabs.com>
2011-02-10 14:28:27 -0800

    updated solver test with new examples and context functions

---
+++ js/plain/solver_test_examples.js
@@ -0,0 +1,20 @@
+/* Solution test examples
+ * */
+
+var TestExamples = [];
+
+TestExamples.push({
+    name: "Bill Calculator 3 and 4",
+    fxn : "3*x+(15*y)",
+    curVarContext : [3,4],
+    parseSol : "3*3+(15*4)",
+    numSol : "69"
+});
+
+TestExamples.push({
+    name: "Bill Calculator 5 and 6",
+    fxn : "3*x+(15*y)",
+    curVarContext : [5,6],
+    parseSol : "3*5+(15*6)",
+    numSol : "105"
+});
